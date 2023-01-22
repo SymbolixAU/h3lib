d319a07e3f4371cd50fa5a33aeb66de4810d6437
dcooley <david.cooley141@gmail.com>
2023-01-22 19:27:21 +1100

    notes

--- src/h3lib.h
+++ src/h3lib.h
@@ -6,6 +6,10 @@
 #ifndef R_H3LIB_H
 #define R_H3LIB_H
 
+
+// h3api.h     -> h3lib.h        ->  h3libapi.h ( C || C++)
+// latLngToCel -> h3LatLngToCell ->  h3lib_latLngToCell || h3lib::latLngToCell
+
 SEXP h3LatLngToCell(SEXP lat, SEXP lon, SEXP res);
 SEXP h3CellToLatLng(SEXP h3);
 
