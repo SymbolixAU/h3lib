b062d7ce1af5cbf026f83dbbdf781dc0af451557
dcooley <=>
2023-08-23 08:32:47 +1000

    docs

--- R/h3lib-package.R
+++ R/h3lib-package.R
@@ -1,3 +1,13 @@
 #' @useDynLib "h3lib", .registration = TRUE
 NULL
 
+
+#' h3lib
+#'
+#' h3lib exposes the source code of 'H3' (written in 'C') to routines that are callable through 'R'.
+#' Detailed examples on how to call these routines are on the website \link{https://github.com/symbolixau/h3lib#how-do-i-call-these-routines}.
+#'
+#' @docType package
+#' @rdname h3lib
+#' @title h3lib
+"_PACKAGE"
